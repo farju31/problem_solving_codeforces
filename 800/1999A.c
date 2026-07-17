5db11b10a5124479ae953dd777762a22d6971c6c
farju31 <farjanaferdosy31@gmail.com>
2026-07-18 01:33:49 +0600

    800 rating problem 1999A

---
+++ 800/1999A.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+
+        int t;
+        scanf("%d",&t);
+
+        while(t--){
+
+            int n;
+            scanf("%d",&n);
+
+            printf("%d\n",((n%10)+(n/10)));
+
+
+        }
+    
+
+}
